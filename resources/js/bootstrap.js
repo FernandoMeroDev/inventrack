5c18a75a6fd83e8575df41aa654bea947d927d87
Kettei <97405768+KetteiPrOt@users.noreply.github.com>
2024-11-13 20:00:25 -0500

    Tuned for deploy

--- resources/js/bootstrap.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-import { Collapse, initTWE } from "tw-elements";
-
-initTWE({ Collapse });
-
-window.axios = axios;
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
--- resources/views/components/file-input.blade.php
+++
@@ -1,3 +0,0 @@
-@props(['disabled' => false])
-
-<input type="file" @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500']) }}>
